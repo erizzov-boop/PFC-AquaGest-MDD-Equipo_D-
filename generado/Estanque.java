fc1e8f6ff10e38bdcaa99779135dcf219c0f6b7d
kcrespoe <kcrespoe@uteq.edu.ec>
2026-07-19 19:46:49 -0500

    Codigo generado de clase Estanque.

---
+++ generado/Estanque.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Estanque {
+    private long id;
+
+    private String nombre;
+
+    private double tamanoHa;
+
+    private String estado;
+
+    private int capacidadMaxSiembra;
+
+    public List<Siembra>  = new ArrayList<Siembra> ();
+
+    public void registrarEstanque() {
+    }
+
+    public void calcularCapacidadMaxima() {
+    }
+
+    public void actualizarEstado() {
+    }
+
+}
