d038efa7f5951c64e846ba8c8aea5a2c17b7d7ea
erizzov-boop <erizzov@uteq.edu.ec>
2026-07-19 19:45:19 -0500

    Código generado de la clase AlertaCrecimiento

---
+++ generado/AlertaCrecimiento.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+
+public class AlertaCrecimiento {
+    private Date fecha;
+
+    private double pesoEsperado;
+
+    private double pesoObservado;
+
+    private String estado;
+
+    private long id;
+
+    public void generarAlerta() {
+    }
+
+    public void atenderAlerta() {
+    }
+
+}
