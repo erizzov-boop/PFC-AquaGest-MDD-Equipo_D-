5f2600cfd15a637b67cd662eee6f198a3014f253
erizzov-boop <erizzov@uteq.edu.ec>
2026-07-19 19:45:54 -0500

    Código generado de la clase Empleado

---
+++ generado/Empleado.java
@@ -0,0 +1,19 @@
+import java.util.Date;
+
+public class Empleado {
+    private String nombre;
+
+    private String cedula;
+
+    private String cargo;
+
+    private String rol;
+
+    private Date fechaIngreso;
+
+    private long id;
+
+    public void registrarEmpleado() {
+    }
+
+}
