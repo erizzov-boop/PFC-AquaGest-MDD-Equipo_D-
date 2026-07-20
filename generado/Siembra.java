05ddb819f208bbfa3a87db02157e54e234072dba
ramaguas-ship-it <ramaguas@uteq.edu.ec>
2026-07-19 19:54:22 -0500

    Código generado de la clase Siembra

---
+++ generado/Siembra.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Siembra {
+    private Date fecha;
+
+    private int cantidadLarvas;
+
+    private String tipoLarva;
+
+    private String laboratorioOrigen;
+
+    private long id;
+
+    private String observaciones;
+
+    public List<Grameo>  = new ArrayList<Grameo> ();
+
+    public List<Empleado>  = new ArrayList<Empleado> ();
+
+    public List<SobrecargaSiembra>  = new ArrayList<SobrecargaSiembra> ();
+
+    public void registrarSiembra() {
+    }
+
+}
