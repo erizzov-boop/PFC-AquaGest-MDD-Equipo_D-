ade5550524638a1ac49a4ffe7a938a1661368262
kcrespoe <kcrespoe@uteq.edu.ec>
2026-07-19 19:47:31 -0500

    Código generado de clase Grameo

---
+++ generado/Grameo.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+
+public class Grameo {
+    private Date fecha;
+
+    private double pesoPromedio;
+
+    private String observaciones;
+
+    private long id;
+
+    public AlertaCrecimiento ;
+
+    public void registrarGrameo() {
+    }
+
+    public void calcularHistorialCrecimiento() {
+    }
+
+}
