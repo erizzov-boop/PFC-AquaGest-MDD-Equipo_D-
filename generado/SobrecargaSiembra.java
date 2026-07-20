b48b2c76bb84dee90b6d2e8c74deee80e02218c3
ramaguas-ship-it <ramaguas@uteq.edu.ec>
2026-07-19 19:55:08 -0500

    Código generado de la clase SobrecargaSiembra

---
+++ generado/SobrecargaSiembra.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+public class SobrecargaSiembra {
+    private double cantidadAdicional;
+
+    private String motivo;
+
+    private Date fechaRegistro;
+
+    private long id;
+
+    public void registrarSobrecarga() {
+    }
+
+    public void validarRango() {
+    }
+
+}
